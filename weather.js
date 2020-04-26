e6349377fa0722ecd55559711e9a2d1278b28c5c
Corina Pelichowski <corina@danitt.com>
2020-04-26 22:05:27 +1000

    completed weather.js file

---
+++ weather.js
@@ -0,0 +1,20 @@
+class Weather {
+    constructor(city, state) {
+        this.apiKey = '93e9303b4c5a4666a2c94622202604';
+        this.city = city;
+        this.state = state;
+    }
+
+    // Fetch weather from API
+    async getWeather() {
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${this.city},${this.state}`);
+        const jsonResponse = await response.json();
+        console.info('weather results in json format', jsonResponse);
+    }
+
+    // Change weather location
+    changeLocation(city, state) {
+        this.city = city;
+        this.state = state;
+    }
+}
